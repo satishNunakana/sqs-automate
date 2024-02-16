cadf15e75225d930f6b5f18d768442b68474a554
satishNunakana <nsk0443@gmail.com>
2024-02-16 09:56:21 +0530

    updated resource

--- sqs-test.tf
+++ sqs-test.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "terraform_queue" {
   name                      = "terraform-example-queue"
   delay_seconds             = 80
   max_message_size          = 2048
-  message_retention_seconds = 86400
+  message_retention_seconds = 8640
   receive_wait_time_seconds = 10
   fifo_queue= false
   content_based_deduplication = false
